dae547710c6a5e53c791553c79d363bc3416b084
Uzzika <dudchenkoleska@gmail.com>
2024-05-18 22:52:22 +0300

    added shaders without rendering

---
+++ Lab3_ray_tracing/Lab3_ray_tracing/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Lab3_ray_tracing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl2 = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // glControl2
+            // 
+            this.glControl2.BackColor = System.Drawing.Color.Black;
+            this.glControl2.Location = new System.Drawing.Point(13, 13);
+            this.glControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.glControl2.Name = "glControl2";
+            this.glControl2.Size = new System.Drawing.Size(452, 424);
+            this.glControl2.TabIndex = 1;
+            this.glControl2.VSync = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 450);
+            this.Controls.Add(this.glControl2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl2;
+    }
+}
+
